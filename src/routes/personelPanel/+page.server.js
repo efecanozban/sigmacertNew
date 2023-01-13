c6d410f2585dc5663392ff54733acf3833cc63da
efecanozban <46239053+efecanozban@users.noreply.github.com>
2023-01-13 15:39:35 +0300

    unimportant test

--- src/routes/personelPanel/+page.server.js
+++ src/routes/personelPanel/+page.server.js
@@ -57,8 +57,26 @@ export const actions = {
     updateGorusme: async ({ request }) => {
         const data = await request.formData()
 
-        let querry;
-        querry = `call update_gorusme(
+        if (data.get("selectedRow") !== 'undefined') {
+            let querry;
+            if (data.get("icerik") !== 'undefined') {
+                querry = `call update_gorusme(
+                    ${userId}, 
+                    '${data.get("gorusulenFirma")}',
+                    ${data.get("gorusmeKanalı")}, 
+                    ${data.get("gorusmeDurumu")}, 
+                    'data:image/png;base64,${data.get("icerik")}', 
+                    '${data.get("gorusulenYetkili")}',
+                    '${data.get("yetkiliTelefon")}',
+                    '${data.get("yetkiliEMail")}',
+                    '${data.get("verilenTeklif")}',
+                    '${data.get("aciklamalar")}',
+                    ${data.get("selectedRow")}
+                )`
+            }
+
+            else {
+                querry = `call update_gorusme(
                     ${userId}, 
                     '${data.get("gorusulenFirma")}',
                     ${data.get("gorusmeKanalı")}, 
@@ -70,8 +88,9 @@ export const actions = {
                     '${data.get("aciklamalar")}',
                     ${data.get("selectedRow")}
                 )`
+            }
 
-        db.getQuerry(querry)
-
+            await db.getQuerry(querry)
+        }
     }
 };
